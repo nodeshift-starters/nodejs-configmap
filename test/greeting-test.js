42579cbb5f35cc48ce54fa99cbf26b18fa3bbd44
Helio Frota <00hf11@gmail.com>
2021-05-24 20:06:43 -0300

    refactor: using mocha for tests
    Using mocha as suggested by reference architecture:
    https://github.com/nodeshift/nodejs-reference-architecture#components

--- test/greeting-test.js
+++ test/greeting-test.js
@@ -1,163 +1,149 @@
-const test = require('tape');
+/* eslint-disable no-undef */
+const assert = require('assert');
 const proxyquire = require('proxyquire');
 const supertest = require('supertest');
 const sinon = require('sinon');
 
-test('test basic greeting api', t => {
-  const clock = sinon.useFakeTimers();
-  const app = proxyquire('../app', {
-    'js-yaml': {
-      safeLoad: data => {
-        return data;
+describe('Greeting API', () => {
+  it('basic test', async () => {
+    const clock = sinon.useFakeTimers();
+    const app = proxyquire('../app', {
+      'js-yaml': {
+        safeLoad: data => {
+          return data;
+        }
+      },
+      fs: {
+        readFile: (location, options, cb) => {
+          const configmap = {
+            message: 'Hello, %s from a ConfigMap !',
+            level: 'INFO'
+          };
+          return cb(null, configmap);
+        }
       }
-    },
-    fs: {
-      readFile: (location, options, cb) => {
-        const configmap = {
-          message: 'Hello, %s from a ConfigMap !',
-          level: 'INFO'
-        };
-        return cb(null, configmap);
-      }
-    }
+    });
+
+    clock.tick(2100);
+    const { body } = await supertest(app)
+      .get('/api/greeting')
+      .expect('Content-Type', /json/)
+      .expect(200);
+
+    assert.strictEqual(body.content, 'Hello, World from a ConfigMap !');
+    clock.restore();
   });
 
-  clock.tick(2100);
-
-  supertest(app)
-    .get('/api/greeting')
-    .expect('Content-Type', /json/)
-    .expect(200)
-    .then(response => {
-      t.equal(response.body.content, 'Hello, World from a ConfigMap !', 'should have a content message');
-      clock.restore();
-      t.end();
+  it('with name', async () => {
+    const clock = sinon.useFakeTimers();
+    const app = proxyquire('../app', {
+      'js-yaml': {
+        safeLoad: data => {
+          return data;
+        }
+      },
+      fs: {
+        readFile: (location, options, cb) => {
+          const configmap = {
+            message: 'Hello, %s from a ConfigMap !',
+            level: 'INFO'
+          };
+          return cb(null, configmap);
+        }
+      }
     });
-});
+    clock.tick(2100);
+    const { body } = await supertest(app)
+      .get('/api/greeting?name=luke')
+      .expect('Content-Type', /json/)
+      .expect(200);
 
-test('test basic greeting api with name', t => {
-  const clock = sinon.useFakeTimers();
-  const app = proxyquire('../app', {
-    'js-yaml': {
-      safeLoad: data => {
-        return data;
-      }
-    },
-    fs: {
-      readFile: (location, options, cb) => {
-        const configmap = {
-          message: 'Hello, %s from a ConfigMap !',
-          level: 'INFO'
-        };
-        return cb(null, configmap);
-      }
-    }
+    assert.strictEqual(body.content, 'Hello, luke from a ConfigMap !');
+    clock.restore();
   });
 
-  clock.tick(2100);
-
-  supertest(app)
-    .get('/api/greeting?name=luke')
-    .expect('Content-Type', /json/)
-    .expect(200)
-    .then(response => {
-      t.equal(response.body.content, 'Hello, luke from a ConfigMap !', 'should have a content message');
-      clock.restore();
-      t.end();
+  it('no configmap yet', async () => {
+    const clock = sinon.useFakeTimers();
+    const app = proxyquire('../app', {
+      'js-yaml': {
+        safeLoad: data => {
+          return data;
+        }
+      },
+      fs: {
+        readFile: (location, options, cb) => {
+          const configmap = {
+            message: 'Hello, %s from a ConfigMap !',
+            level: 'INFO'
+          };
+          return cb(null, configmap);
+        }
+      }
     });
-});
 
-test('test greeting api - no configmap yet', t => {
-  const clock = sinon.useFakeTimers();
-  const app = proxyquire('../app', {
-    'js-yaml': {
-      safeLoad: data => {
-        return data;
-      }
-    },
-    fs: {
-      readFile: (location, options, cb) => {
-        const configmap = {
-          message: 'Hello, %s from a ConfigMap !',
-          level: 'INFO'
-        };
-        return cb(null, configmap);
-      }
-    }
+    const { body } = await supertest(app)
+      .get('/api/greeting')
+      .expect(500);
+
+    assert.strictEqual(body.content, 'no config map');
+    clock.restore();
   });
 
-  supertest(app)
-    .get('/api/greeting')
-    .expect(500)
-    .then(response => {
-      t.equal(response.body.content, 'no config map', 'should have a content message');
-      clock.restore();
-      t.end();
+  it('check for configmap, but nothing', async () => {
+    const clock = sinon.useFakeTimers();
+    const app = proxyquire('../app', {
+      'js-yaml': {
+        safeLoad: data => {
+          return data;
+        }
+      },
+      fs: {
+        readFile: (location, options, cb) => {
+          return cb(null, null);
+        }
+      }
     });
-});
 
-test('test basic greeting api - check for configmap, but nothing', t => {
-  const clock = sinon.useFakeTimers();
-  const app = proxyquire('../app', {
-    'js-yaml': {
-      safeLoad: data => {
-        return data;
-      }
-    },
-    fs: {
-      readFile: (location, options, cb) => {
-        return cb(null, null);
-      }
-    }
+    clock.tick(2100);
+    const { body } = await supertest(app)
+      .get('/api/greeting')
+      .expect('Content-Type', /json/)
+      .expect(500);
+
+    assert.strictEqual(body.content, 'no config map');
+    clock.restore();
   });
 
-  clock.tick(2100);
-
-  supertest(app)
-    .get('/api/greeting')
-    .expect('Content-Type', /json/)
-    .expect(500)
-    .then(response => {
-      t.equal(response.body.content, 'no config map', 'should have a content message');
-      clock.restore();
-      t.end();
+  it('logger change', async () => {
+    const clock = sinon.useFakeTimers();
+    const app = proxyquire('../app', {
+      'js-yaml': {
+        safeLoad: data => {
+          return data;
+        }
+      },
+      winston: {
+        level: 'info',
+        info: () => {},
+        debug: () => {}
+      },
+      fs: {
+        readFile: (location, options, cb) => {
+          return cb(null, {
+            message: 'Hello, %s from a ConfigMap !',
+            level: 'DEBUG'
+          });
+        }
+      }
     });
-});
+    clock.tick(2100);
 
-test('test logger change', t => {
-  const clock = sinon.useFakeTimers();
-  const app = proxyquire('../app', {
-    'js-yaml': {
-      safeLoad: data => {
-        return data;
-      }
-    },
-    winston: {
-      level: 'info',
-      info: () => {},
-      debug: () => {}
-    },
-    fs: {
-      readFile: (location, options, cb) => {
-        return cb(null, {
-          message: 'Hello, %s from a ConfigMap !',
-          level: 'DEBUG'
-        });
-      }
-    }
+    const { body } = await supertest(app)
+      .get('/api/greeting?name=luke')
+      .expect('Content-Type', /json/)
+      .expect(200);
+
+    assert.strictEqual(body.content, 'Hello, luke from a ConfigMap !');
+    clock.restore();
   });
-
-  clock.tick(2100);
-
-  clock.tick(2100);
-
-  supertest(app)
-    .get('/api/greeting?name=luke')
-    .expect('Content-Type', /json/)
-    .expect(200)
-    .then(response => {
-      t.equal(response.body.content, 'Hello, luke from a ConfigMap !', 'should have a content message');
-      clock.restore();
-      t.end();
-    });
 });
