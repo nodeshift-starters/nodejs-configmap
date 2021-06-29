8396fe57db2c17f27080e8d8d8d08584c35659dc
Lucas Holmquist <lholmqui@redhat.com>
2021-06-29 13:51:51 -0400

    fix: safeLoad is deprecated and is now just load
    fixes #180

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ const jsyaml = require('js-yaml');
 function retrieveConfigfMap () {
   return readFile(process.env.NODE_CONFIGMAP_PATH, { encoding: 'utf8' }).then(configMap => {
     // Parse the configMap, which is yaml
-    const configMapParsed = jsyaml.safeLoad(configMap);
+    const configMapParsed = jsyaml.load(configMap);
     return configMapParsed;
   });
 }
